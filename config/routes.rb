33fe24fbc5e02f977432eeb3c6f518cdd90fab3c
JuanNicolasGomez <nicolas.gm243@gmail.com>
2019-02-27 14:47:48 -0500

    added sample data

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
-  resources :records
-  resources :artists
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  namespace :api do
+    namespace :v1 do
+    resources :records
+    resources :artists
+    end
+  end
+
+  root to: "home#index"
+
+  post "refresh", controller: :refresh, action: :create
+  post "signin", controller: :signin, action: :create
+  post "signup", controller: :signup, action: :create
+  delete "singin", controller: :signin, action: :destroy
+
+  
 end
